9469902d14e098375130b2f84b691d28c5e886d7
Vasundhara ghose <vasu.ghs@gmail.com>
2018-02-23 01:11:05 +0530

    Update TriplateSum.java

--- Week2/TriplateSum.java
+++ Week2/TriplateSum.java
@@ -13,48 +13,45 @@ public class TriplateSum
 		 Arrays.sort(ar); //sorting array
 		 for(int i=0;i<n-2;i++) //traversing array for top n-2 elements 
 		 {
-			 left=i+1; //next bigger element from left
-			 right=n-1;// most largest element
+			  left=i+1; //next bigger element from left
+			  right=n-1;// most largest element
 		 
-		  while(left<right) //till elements indices don't coincide
-		  {
-			  int sum=ar[i]+ar[left]+ar[right];
-			  if(sum==0) // check wheather they are eligible to make ZERO or not ;
+			  while(left<right) //till elements indices don't coincide
 			  {
-				 // System.out.println("<"+ar[i]+","+ar[left]+","+ar[right]+">");
-				  
-				  SortedSet<Integer> temp=new TreeSet<>(); //new temp list
-				  temp.add(ar[i]);        //add element in list
-				  temp.add(ar[left]);
-				  temp.add(ar[right]);
-				  left++;
-				  right--;
-				
-				  if(!bags.contains(temp)) //uniqueness test
+				  int sum=ar[i]+ar[left]+ar[right];
+				  if(sum==0) // check wheather they are eligible to make ZERO or not ;
 				  {
-					  bags.add(temp);     //add triplet into it
+					 
+					  SortedSet<Integer> temp=new TreeSet<>(); //new temp list
+					  temp.add(ar[i]);        //add element in list
+					  temp.add(ar[left]);
+					  temp.add(ar[right]);
+					  left++;
+					  right--;
+
+					  if(!bags.contains(temp)) //uniqueness test
+					  {
+						  bags.add(temp);     //add triplet into it
+					  }
+
+
+
 				  }
-				 
-				
-				  
-			  }
-			  else if(sum<0) //if sum less than zero then we need larger elements for sum=0 
-			  {
-				  left++;   //so move towards left
-			  }	  
-			  else
-			  {
-				  right--; //else move towards right
-				  
-			  }		  
-			 
-		   }
-		
+				  else if(sum<0) //if sum less than zero then we need larger elements for sum=0 
+				  {
+					  left++;   //so move towards left
+				  }	  
+				  else
+				  {
+					  right--; //else move towards right
+
+				  }		  
+
+			   }
+
 	   }
-		 if(!bags.isEmpty())
-			 return bags;
-		 else		 
-			 return null;
+	return bags;
+		 
 	}
 	
 	/*public static void main(String arg[])
